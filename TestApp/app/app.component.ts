e6b07e93bea3fbf1ffe923f3fa333785bb226b46
varun <varunbilurkar@gmailcom>
2017-04-13 13:12:38 +0530

    added angular 2

---
+++ TestApp/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `<h1>Welcome to {{name}}</h1><h4> this is tanay</h4>`,
+})
+export class AppComponent  { name = 'angular2 on mobile'; }
